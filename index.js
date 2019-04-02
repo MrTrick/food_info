4bb2789222524da53221800062610ee7c659ac22
Patrick Barnes <bitbucket@mindbleach.com>
2019-04-03 07:38:43 +1100

    Added header row, improved parsing, updated Bootstrap version

--- index.js
+++ index.js
@@ -1,83 +1,76 @@
 
 $(document).ready(function() {
   'use strict';
-  /**
-   * Store food data for the application
-   * @type {object}
-   */
-  var data;
 
-  function toId(str) { return str.toLowerCase().trim().replace(/\W+/g,'_'); }
-  function toKeywords(str) { str=str.replace(/\W+/g,' ').trim(); return str?_.uniq(str.split(' ')):[]; }
-  function parseList(str) { return str.trim().split(/ *, */); }
-  /**
-   * Parse the data file into the final form for display
-   * @param  {string} text Text body
-   * @param  {string} name Name of the text file, for error reporting
-   * @return {object}      Large map of data keyed by id
-   */
-  function parseData(text, name) {
-    var errors = [];
-    var data = {};
-    //Split into lines
-    text.trim().split(/\r?\n/).forEach(function(line,n) {
-      //Split into fields
-      if (!(line = line.trim())) return;
-      var f = line.split('\t');
-      if (f.length < 3) { errors.push(name+":"+n+" Must have at least 3 fields with '|' characters between."); return; }
-      //Parse fields
-      var id=toId(f[0]), title=f[0];
-      var status=f[1];
-      var types=parseList(f[2]);
-      var description=f[3] || false;
-      var exceptions=f[4] ? parseList(f[4]) : false;
-      var keywords = toKeywords(description||title);
-      //Create entry
-      data[id] = {id:id,title:title,n:n,status:status,description:description,keywords:keywords,types:types,exceptions};
-    });
-    //Cross-link item exceptions
-    _.each(data, function(item) {
-      item.exceptions && (item.exceptions = item.exceptions.map(function(ex) {
-        var id = toId(ex);
-        if (!(id in data))errors.push(name+":"+item.n+" Exception references item "+ex+", not found in list.");
-        else return data[id];
-      }));
-    });
-    if (errors.length) throw errors.join("\n");
-    else return data;
-  }
+  //1. Fetch and parse the foods
+  var src = $('#foodlist').attr("src");
+  $.ajax(src).done(function(text) {
+    var foods = parseFoods(text);
 
-  //Handle actions
+    //2. Collate the types of foods listed
+    var types = _.map(foods,'types').reduce(function(a,b){return _.union(a,b);},[]);
+
+    //3. Report
+    console.log("Parsed "+_.size(foods)+" foods.");
+    console.log("Found "+types.length+" categories.");
+
+    //4. Render the food types into the type section
+    var $type_container = $('#types');
+    types.forEach(function(type) {
+      var $type = $(template_type({type:type})).data({type:type});
+      $type.data('type',type);
+      $type.click(onSelectType);
+      $type_container.append($type);
+    });
+
+    //5. Render the food items into the items section
+    var $items_container = $('#items');
+    _.forEach(foods, function(item) {
+      var $item = $(template_card(item));
+      $item.data(item);
+      $items_container.append($item);
+    });
+
+    //6. Add other event handlers
+    $('#search').keyup(_.debounce(onSearchTerm, 500));
+    $('#clear-filter').click(onClearSearch);
+    $(window).scroll(function() {
+       if ($(this).scrollTop() > 100) $('#back-to-top').fadeIn();
+       else $('#back-to-top').fadeOut();
+   });
+   $('#button-top').click(function() {
+       $("html, body").animate({ scrollTop: 0 }, 600);
+       return false;
+   });
+  });
+
+  //============================================================================
+
+  /**
+   * Action Handler: Called when a food type is clicked
+   */
   function onSelectType(evt) {
-    var $type = $(this), type=evt.target.innerText;
+    var $type = $(this);
+    var type = $(this).data('type');
+    //Unselect the previous type button
     $('#types .btn-primary').removeClass('btn-primary').addClass('btn-light');
+    //Select this button
     $type.removeClass('btn-light').addClass('btn-primary');
     doSearch($('#search').text(), type);
   };
+
+  /**
+   * Action Handler: Called when text is changed in the search bar
+   */
   function onSearchTerm(evt) {
     var text = evt.target.value;
     doSearch(text, $('#types .btn-primary').text());
   };
 
-  function doSearch(text, type) {
-    var keywords = toKeywords(text);
-    if(!keywords.length && !type) return clearSearch();
-
-    //Update visibility of objects
-    $('#items').children().each(function() {
-      var id = this.id, item = data[id];
-      //Does item match the filter?
-      function noMatch(w) { var subj =item.title+' '+item.description; return subj.indexOf(w)===-1; }
-      if ((type && !_.includes(item.types, type)) || _.find(keywords, noMatch)) $(this).fadeOut();
-      else $(this).fadeIn();
-    });
-    //Show the 'clear search' button
-    $('#clear-filter').fadeIn();
-    //Update the highlighting
-    $('#items').unmark({done:function() { $('#items').mark(keywords); }});
-  }
-
-  function clearSearch() {
+  /**
+   * Action Handler: Called when the search bar and filtering should be cleared.
+   */
+  function onClearSearch() {
     $('#search').val('');
     $('#types .btn-primary').removeClass('btn-primary').addClass('btn-light');
     $('#clear-filter').fadeOut();
@@ -85,55 +78,134 @@ $(document).ready(function() {
     $('#items').unmark();
   }
 
-  //Fetch the data and render cards
-  var src = $('#foodlist').attr("src");
-  $.ajax(src)
-  .done(function(text) {
-    //Ooh, data
-    data = parseData(text,src);
-    var types = _.reduce(_.map(data,'types'),function(a,b){return _.union(a,b);},[]);
-    console.log("Parsed "+_.size(data)+" foods.");
-    console.log("Found "+types.length+" categories.");
+  //============================================================================
 
-    //Populate the types
-    var $types = $('#types');
-    _.each(types,function(type) { type = {id:toId(type),name:type};
-      var $type = $(template_type(type));
-      $type.click(onSelectType);
-      $types.append($type);
+  /**
+   * Filter the items to those matching the text and type
+   * @param  {string} text Free text entry
+   * @param  {string} type Optionally a food type
+   */
+  function doSearch(text, type) {
+    //Filter and validate the inputs
+    var keywords = toKeywords(text);
+    if(!keywords.length && !type) return onClearSearch();
+
+    //Update visibility of objects
+    $('#items').children().each(function() {
+      var item = $(this).data();
+
+      //If item does NOT match the filter, hide it! Otherwise show.
+      function noMatch(w) { var subj = item.title+' '+item.description; return subj.indexOf(w)===-1; }
+      if ((type && !_.includes(item.types, type)) || _.find(keywords, noMatch)) $(this).fadeOut();
+      else $(this).fadeIn();
     });
 
-    //Insert items into the document
-    var $items = $('#items');
-    _.each(data, function(item) {
-      var $item = $(template_card(item));
-      $items.append($item);
+    //Make the 'clear search' button visible
+    $('#clear-filter').fadeIn();
+
+    //Update keyword highlighting
+    $('#items').unmark({done:function() { $('#items').mark(keywords); }});
+  }
+
+  //============================================================================
+
+  /**
+   * Parse and validate the food definitions
+   * @param  {string} text TSV-format definitions with header row
+   * @return {object}      Foods keyed by ID
+   */
+  function parseFoods(text) {
+    var foods = {}, errors = [];
+
+    //Convert the file from TSV, validate, and store by id
+    parseTSV(text).forEach(function(f, n) {
+      //Require fields
+      if (!f.title || !f.status || !f.types) errors.push("Record "+n+": Must define at least title, status, type");
+
+      //Handle list fields
+      if (f.types) f.types = f.types.split(/ *, */);
+      if (f.exceptions) {
+        f.exceptions = f.exceptions.split(/ *, */).map(function(ex) {
+          return { id: toId(ex), title: ex };
+        });
+      }
+
+      //Add extra fields
+      f.n = n;
+      f.id = toId(f.title);
+      f.keywords = toKeywords(f.description||f.title);
+
+      foods[f.id] = f;
+    })
+    //Validate cross-links between food items
+    _.forEach(foods, function(f) {
+      //Check that each exception links to a valid food.
+      f.exceptions && f.exceptions.forEach(function(ex) {
+        if (!foods[ex.id]) errors.push("Record "+f.n+": Exception references item "+ex+", not found in list.");
+      });
     });
 
-    //Other event handlers
-    $('#search').keyup(_.debounce(onSearchTerm, 500));
-    $('#clear-filter').click(clearSearch);
-    $(window).scroll(function(){
-       if ($(this).scrollTop() > 100) $('#back-to-top').fadeIn();
-       else $('#back-to-top').fadeOut();
-   });
-   $('#button-top').click(function(){
-       $("html, body").animate({ scrollTop: 0 }, 600);
-       return false;
-   });
-  });
+    //Check the collated errors and report if any failures.
+    if (errors.length) throw errors.join("\n");
+    else return foods;
+  }
+
+  /**
+   * Parse the TSV text into an array of objects, with keys determined by the header row.
+   * Skips empty rows or empty fields.
+   *
+   * @param  {string} text       Raw TSV file
+   * @return {array}
+   */
+  function parseTSV(text) {
+    //Convert the text into a 2D array of rows and values
+    var rows = text.trim().split(/ *\r?\n */).map(function(line) {
+      return line && line.split(/ *\t */);
+    }).filter(_.identity);
+
+    //Take the top row as keys
+    var keys = rows.shift();
+
+    //Combine with the remainder to make an array of records.
+    return rows.map(function(values) {
+      return _.zipObject(keys, values);
+    });
+  }
+
+  /**
+   * Convert a food name into a normalized ID form
+   * @param  {string} name
+   * @return {string}
+   */
+  function toId(name) {
+     return name.toLowerCase().trim().replace(/\W+/g,'_');
+  }
+
+  /**
+   * Convert input text into a list of normalised keywords
+   * @param  {string} text
+   * @return {array}
+   */
+  function toKeywords(text) {
+    text=text.replace(/\W+/g,' ').trim();
+    return text ? _.uniq(text.split(' ')) : [];
+  }
+
+  //============================================================================
+
+  //Compile the templates
+  var template_card = _.template(
+    $('#template_card').html(),
+    {imports: {
+      border_class: {good:"border-success", maybe:"border-warning", bad:"border-danger"},
+      label_class: {good:"text-success", maybe:"text-warning", bad:"text-danger"},
+      label: {good:"SAFE", maybe:"MODERATION", bad:"UNSAFE"},
+      exception_class: {good:"text-danger", maybe:"", bad:"text-success"},
+    }}
+  );
+
+  var template_type = _.template(
+    $('#template_type').html()
+  );
+
 });
-
-//Compile the templates
-var template_card = _.template(
-  $('#template_card').html(),
-  {imports: {
-    border_class: {good:"border-success", maybe:"border-warning", bad:"border-danger"},
-    label_class: {good:"text-success", maybe:"text-warning", bad:"text-danger"},
-    label: {good:"SAFE", maybe:"MODERATION", bad:"UNSAFE"},
-    exception_class: {good:"text-danger", maybe:"", bad:"text-success"},
-  }}
-);
-var template_type = _.template(
-  $('#template_type').html()
-);
